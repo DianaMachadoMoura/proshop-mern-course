fe46c66920ee56b7cbd367c48c9f4e50f030b97c
Diana Moura <diana.machado@live.com.pt>
2025-07-02 18:25:22 +0200

    add product ratings and do small refactor in product component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,21 +1,29 @@
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import Rating from './Rating';
+
 const Product = ({ product }) => {
+  const { _id, image, name, rating, numReviews, price } = product;
+
   return (
     <Card className="my-3 p-3 rounded">
-      <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
+      <Link to={`/product/${_id}`}>
+        <Card.Img src={image} variant="top" />
       </Link>
 
       <Card.Body>
-        <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
-            <strong>{product.name}</strong>
+        <Link to={`/product/${_id}`}>
+          <Card.Title as="div" className="product-title">
+            <strong>{name}</strong>
           </Card.Title>
         </Link>
 
-        <Card.Text as="h3">${product.price}</Card.Text>
+        <Card.Text as="div">
+          <Rating value={rating} text={`${numReviews} reviews`} />
+        </Card.Text>
+
+        <Card.Text as="h3">${price}</Card.Text>
       </Card.Body>
     </Card>
   );
