c68d83ff37c1ceca59e8e3a20106230ac66a7d37
Diana Moura <diana.machado@live.com.pt>
2025-07-02 17:19:07 +0200

    implement react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
+import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
-import HomeScreen from './screens/HomeScreen';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -10,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
